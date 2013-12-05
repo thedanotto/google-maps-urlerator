4f1759bda398d64aabaab73f43dc3d87d73bc163
Dan Otto <theDanOtto@gmail.com>
2013-12-04 23:38:16 -0600

    this site looks sexy now

--- urlerator/views.py
+++ urlerator/views.py
@@ -9,12 +9,14 @@ def home(request):
         
     if request.POST:
         address = request.POST['address'] #it gets the post data by Name
+        
         print address
-        google_directions_url = 'https://maps.google.com/maps?ie=UTF-8&gl=us&daddr=%s&f=d&fb=1&dirflg=d' %(address)
+        url_output = 'https://maps.google.com/maps?ie=UTF-8&gl=us&daddr=%s&f=d&fb=1&dirflg=d' %(address)
+        html_output = "<a href='%s'>Directions</a>" %(url_output)
         if url_form.is_valid():
             form1 = url_form.save(commit=False)
             form1.from_address = address
-            form1.url_output = google_directions_url
+            form1.url_output = url_output
             form1.save()
             print form1.id
     return render_to_response('home.html', locals(), context_instance=RequestContext(request))
